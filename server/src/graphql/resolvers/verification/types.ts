6b8d84e274c84e5037494e16443f59ecec2d2b6b
Edward Miller <edmiller.me@gmail.com>
2023-11-17 21:59:44 +1300

    verification requests

---
+++ server/src/graphql/resolvers/verification/types.ts
@@ -0,0 +1,6 @@
+export interface GetVerificationRequestsArgs {
+  input: {
+    skip: number;
+    take: number;
+  };
+}
--- server/src/graphql/resolvers/verification/verificationResolvers.ts
+++ server/src/graphql/resolvers/verification/verificationResolvers.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from "express";
 import { prisma } from "../../../database";
 import { Verification } from "@prisma/client";
+import { GetVerificationRequestsArgs } from "./types";
 
 export const verificationResolvers = {
   Query: {
     getVerificationRequests: async (
       _root: undefined,
-      {},
+      { input }: GetVerificationRequestsArgs,
       { _, req, res }: { _: undefined; req: Request; res: Response }
     ): Promise<Verification[]> => {
       try {
-        const verifications = await prisma.verification.findMany();
+        const verifications = await prisma.verification.findMany({
+          skip: input.skip,
+          take: input.take,
+          orderBy: {
+            created_At: "desc",
+          },
+        });
 
         return verifications;
       } catch (error) {

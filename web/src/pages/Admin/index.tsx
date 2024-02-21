2cbf4ae71ca8f2a9b4bdae7e7c9cc4e1c3a5a6e0
Edward Miller <edmiller.me@gmail.com>
2024-02-21 16:21:32 +1300

    changed Admin currentUser component

--- web/src/pages/Admin/index.tsx
+++ web/src/pages/Admin/index.tsx
@@ -47,35 +47,7 @@ export const Admin = () => {
   };
 
   if (currentUser && currentUser.providerData[0].providerId !== "password") {
-    return (
-      <div className="min-h-screen flex flex-col justify-center items-center">
-        <User
-          size={40}
-          weight="bold"
-          className="text-ch-dark dark:text-ch-light"
-        />
-        <h1 className="mt-4 text-3xl font-bold tracking-tight">
-          Not Logged In
-        </h1>
-        <p className="mt-6 text-base leading-7 mx-10 text-center sm:mx-3">
-          You need to log in using an admin account to use the admin dashboard
-        </p>
-        <div className="flex justify-center items-center space-x-5 mt-10">
-          <Link
-            to="/admin/login"
-            className="underline text-ch-red hover:text-ch-red-hover"
-          >
-            Login
-          </Link>
-          <Link
-            to="/"
-            className="underline text-ch-red hover:text-ch-red-hover"
-          >
-            Home
-          </Link>
-        </div>
-      </div>
-    );
+    return <Login />;
   }
 
   if (!currentUser) {

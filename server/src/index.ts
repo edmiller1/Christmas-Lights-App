bf538e7740ad6fe95a986e47be31d9ee9937eb6c
Edward Miller <edmiller.me@gmail.com>
2024-03-02 18:15:17 +1300

    removed unused import

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,6 @@ import { ApolloServer } from "@apollo/server";
 
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { resolvers, typeDefs } from "./graphql";
-import { seedDb } from "../temp/seed";
 
 (async function () {
   const port = Number(process.env.PORT);

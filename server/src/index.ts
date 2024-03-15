ceb05d4d4ea5b1161b4d53c856a85353e4d7c9e8
Edward Miller <edmiller.me@gmail.com>
2024-03-15 18:05:47 +1300

    installed kinde Autha nd removed js files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,16 @@
 require("dotenv").config();
+const kindeNode = require("kinde-node");
 
 import { ApolloServer } from "@apollo/server";
 
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { resolvers, typeDefs } from "./graphql";
+
+let authorize: any;
+
+(async () => {
+  authorize = await kindeNode(process.env.KINDE_DOMAIN);
+})();
 
 (async function () {
   const port = Number(process.env.PORT);
@@ -18,6 +25,22 @@ import { resolvers, typeDefs } from "./graphql";
     listen: { port },
     context: async ({ req, res }) => ({ req, res }),
   });
+
+  context: async ({ req, res }: { req: Request; res: Response }) => {
+    // auth check on every request
+    const user = new Promise((resolve, reject) => {
+      authorize(req, (err: any, user: any) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(user);
+      });
+    });
+
+    return {
+      user,
+    };
+  };
 
   //seedDb();
 

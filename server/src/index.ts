cec4c63fbef78fbc61345f6e946c76dbfefb85f1
Edward Miller <edmiller.me@gmail.com>
2024-03-15 22:49:29 +1300

    removed kinde

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,9 @@
 require("dotenv").config();
-//@ts-ignore
-const kindeNode = require("kinde-node");
 
 import { ApolloServer } from "@apollo/server";
 
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { resolvers, typeDefs } from "./graphql";
-
-let authorize: any;
-
-(async () => {
-  authorize = await kindeNode(process.env.KINDE_DOMAIN);
-})();
 
 (async function () {
   const port = Number(process.env.PORT);
@@ -21,27 +13,10 @@ let authorize: any;
     resolvers,
   });
 
-  //@ts-ignore
   await startStandaloneServer(server, {
     listen: { port },
     context: async ({ req, res }) => ({ req, res }),
   });
-
-  context: async ({ req, res }: { req: Request; res: Response }) => {
-    // auth check on every request
-    const user = new Promise((resolve, reject) => {
-      authorize(req, (err: any, user: any) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(user);
-      });
-    });
-
-    return {
-      user,
-    };
-  };
 
   //seedDb();
 
